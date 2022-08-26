9f9085e39895b3a87b385ed7709a118b5f699905
Aputl <freelance_yuuichi_onoue@yahoo.co.jp>
2022-08-26 15:17:37 +0900

    デプロイ向け修正2

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,7 +5,7 @@ const header = () => {
   return (
     <header className="text-white p-4 bg-gradient-to-r from-black to-red-900">
     <nav className="flex justify-between mx-auto container items-center">
-        <div className="text-4xl"><h1><a href="/">Midjourney Gallary</a></h1></div>
+        <div className="text-4xl"><h1><a href="/Midjourney-gallary/About">Midjourney Gallary</a></h1></div>
         <div className="text-2xl">
             <a className="mx-2" href="./About">About</a>
         </div>
